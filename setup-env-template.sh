609244a4d1ab4bbd88886e483989f38dd9924039
Austin Hunt <huntaj@g.cofc.edu>
2021-10-17 19:05:52 -0400

    add template setup-env script

---
+++ setup-env-template.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script to set up your environment variables. Run this before executing ansible playbooks/vagrant.
+# Copy this to a new file in the same folder called setup-env.sh
+export AWS_ACCESS_KEY_ID="GET THIS FROM AWS; REPLACE ME"
+export AWS_SECRET_ACCESS_KEY="GET THIS FROM AWS; REPLACE ME"
