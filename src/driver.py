0ee5ec2406200bfb61f33cb07cb7f8f6bfe7810b
Austin Hunt <huntaj@g.cofc.edu>
2021-11-14 17:51:09 -0500

    add energy data and update producer to accept --read_data_from_file CSVFILE and --batch_size NUMRECORDSPERMESSAGE

--- src/driver.py
+++ src/driver.py
@@ -30,17 +30,28 @@ class Driver:
             self.consumer.connect_couchdb()
         self.consumer.consume(save_data=save_data)
 
-    def run_producer(self,topic='stock-market-data', producer_alias="Producer 1",stock_symbol="AMZN",
-        sleep_interval=1, verbose=False,num_messages=100):
+    def run_producer(self,topic='stock-market-data', producer_alias="Producer 1", read_data_from_file=None,
+        batch_size=None, stock_symbol="AMZN", sleep_interval=1, verbose=False,num_messages=100):
         """ Method to drive a Kafka Producer process (run this from a local VM in VirtualBox) """
         self.info("Running Kafka Producer...")
-        self.producer = Producer(
-            bootstrap_server=self.bootstrap_server,
-            producer_alias=producer_alias,
-            stock_symbol=stock_symbol,
-            sleep_interval=sleep_interval,
-            verbose=verbose
-        )
+        if read_data_from_file:
+            # use file (already validated), not in memory custom data
+            self.producer = Producer(
+                bootstrap_server=self.bootstrap_server,
+                producer_alias=producer_alias,
+                sleep_interval=sleep_interval,
+                verbose=verbose,
+                read_data_from_file=read_data_from_file,
+                batch_size=batch_size
+            )
+        else:
+            self.producer = Producer(
+                bootstrap_server=self.bootstrap_server,
+                producer_alias=producer_alias,
+                stock_symbol=stock_symbol,
+                sleep_interval=sleep_interval,
+                verbose=verbose
+            )
         self.producer.produce(num_messages=num_messages, topic=topic)
 
     def setup_logging(self, verbose):
@@ -102,13 +113,24 @@ parser.add_argument('-v', '--verbose', help='increase output verbosity', action=
 parser.add_argument('-t', '--topic',
     help='topic to produce (if running producer with -p) or consume (if running consumer with -c)',
     type=str,required=True)
+
 parser.add_argument('-p', '--run_producer', help='whether to run producer', action='store_true')
 parser.add_argument('-pa', '--producer_alias', default='Producer 1',
-    help='friendly alias/name of producer', type=str)
+    help='WHEN RUNNING PRODUCER WITH -p|--producer, friendly alias/name of producer', type=str)
 parser.add_argument('-n', '--num_messages', type=int, default=100, help='number of messages to produce')
 parser.add_argument('-s', '--sleep_interval', default=1, type=int,
-    help='number of seconds to sleep between each message sent')
-parser.add_argument('-ss', '--stock_symbol', default='AMZN', help='stock symbol to produce data for', type=str)
+    help='WHEN RUNNING PRODUCER WITH -p|--producer, number of seconds to sleep between each message sent')
+parser.add_argument('-ss', '--stock_symbol', default='AMZN', help=(
+    'WHEN RUNNING PRODUCER WITH -p|--producer, stock symbol to produce data for'), type=str)
+parser.add_argument('-r', '--read_data_from_file', help=(
+    'WHEN RUNNING PRODUCER WITH -p|--producer, use -r CSV_FILENAME to tell the producer '
+    'to read data from a CSV file instead of producing its '
+    'own custom data; must be a valid .csv file path on your machine'
+    ), type=str)
+parser.add_argument('-b', '--batch_size', help=(
+    'WHEN RUNNING PRODUCER WITH -p|--producer AND -r|--read_data_from_file CSV_FILENAME, use --batch_size '
+    'to tell producer how many records of the CSV to include in each message; REQUIRED'
+    ), type=int)
 
 parser.add_argument('-c', '--run_consumer', help='whether to run consumer', action='store_true')
 parser.add_argument('-d', '--dump', help='whether consumer should dump data/save to couchdb, only run with -c', action='store_true')
@@ -139,14 +161,39 @@ driver.debug(
 )
 
 if args.run_producer:
-    driver.run_producer(
-        topic=args.topic,
-        producer_alias=args.producer_alias ,
-        stock_symbol=args.stock_symbol ,
-        num_messages=args.num_messages,
-        sleep_interval=args.sleep_interval,
-        verbose=args.verbose
-    )
+    if args.read_data_from_file:
+        if not args.batch_size:
+            raise Exception(
+                'when using -r|--read_data_from_file, you MUST include '
+                '-b|--batch_size to tell producer how many records of '
+                'the CSV to include in each message')
+        # Try to read the file provided ; throw error if it fails
+        try:
+            driver.info('Using provided data file instead of custom producer data in memory')
+            f = open(args.read_data_from_file,'r')
+            driver.debug(f'File {args.read_data_from_file} has {len(f.readlines)} lines')
+            f.close()
+            driver.run_producer(
+                topic=args.topic,
+                producer_alias=args.producer_alias ,
+                read_data_from_file=args.read_data_from_file,
+                batch_size=args.batch_size,
+                sleep_interval=args.sleep_interval,
+                verbose=args.verbose
+            )
+        except Exception as e:
+            driver.error(e)
+            sys.exit(1)
+    else:
+        driver.info('Using custom producer data (in memory), not a data file')
+        driver.run_producer(
+            topic=args.topic,
+            producer_alias=args.producer_alias ,
+            stock_symbol=args.stock_symbol ,
+            num_messages=args.num_messages,
+            sleep_interval=args.sleep_interval,
+            verbose=args.verbose
+        )
 elif args.run_consumer:
     driver.run_consumer(
         topic=args.topic,
--- src/lib/producer.py
+++ src/lib/producer.py
@@ -11,22 +11,26 @@
 #
 
 import json
+from os import read
 import time
 import datetime
 from kafka import KafkaProducer
 from kafka.errors import KafkaTimeoutError
 import yfinance
 import logging
+import csv
 
 # We can make this more sophisticated/elegant but for now it is just
 # hardcoded to the setup I have on my local VMs
 
 
 class Producer:
-    def __init__(self, bootstrap_server="localhost", producer_alias="Producer1",
-        stock_symbol="AMZN", sleep_interval=1, verbose=False):
+    def __init__(self, bootstrap_server="localhost", producer_alias="Producer1", read_data_from_file=None,
+        batch_size=None, stock_symbol="AMZN", sleep_interval=1, verbose=False):
         # Host to contact to bootstrap initial cluster metadata.
         # Needs to have at least one broker that will respond to a Metadata API Request.
+        self.read_data_from_file = read_data_from_file
+        self.batch_size = batch_size
         self.producer_alias = producer_alias
         self.stock_symbol = stock_symbol
         self.sleep_interval = sleep_interval
@@ -66,27 +70,56 @@ class Producer:
     def error(self, msg):
         self.logger.error(msg, extra=self.prefix)
 
+    def _split_list_into_n_chunks(self, lst, n):
+        """Yield successive n-sized chunks from lst."""
+        return [lst[i:i+n] for i in range(0, len(lst), n)]
+
     def produce(self, num_messages=100, topic='stock-market-data'):
         """ Produce data / send data into pipeline <count> times
         Args:
         - count (int): number of times to send data """
         self.info(f'Beginning to produce data')
-        for i in range(num_messages):
-            self.debug(f'Producing message {i}, stock_symbol={self.stock_symbol}')
-            message = {
-                'timestamp': datetime.datetime.now().isoformat(),
-                'stock_symbol': self.stock_symbol,
-                'current_share_price': yfinance.Ticker(self.stock_symbol).info['currentPrice'],
-                'producer_alias': self.producer_alias,
-                'message_index': i
-            }
-            message = bytes(json.dumps(message), 'ascii')
-            try:
-                self.kafka_producer.send(topic=topic, value=message)
-                self.kafka_producer.flush() # try to empty sending buffer
-            except KafkaTimeoutError as e:
-                self.error(f'Timeout error when sending message: {str(e)}')
-            except Exception as e:
-                self.error(f'Exception when sending message: {str(e)}')
-            time.sleep(self.sleep_interval)
+        if self.read_data_from_file:
+            self.info(f'Reading data from CSV file {self.read_data_from_file}')
+            with open(self.read_data_from_file, 'r') as csvfile:
+                # make reader subscriptable by wrapping it into a list to help with batching
+                reader = list(csv.reader(csvfile, delimiter=','))
+                csvfile_chunks = self._split_list_into_n_chunks(reader, self.batch_size)
+                for i, data_chunk in enumerate(csvfile_chunks):
+                    message = {
+                        'chunk': data_chunk,
+                        'timestamp': datetime.datetime.now().isoformat(),
+                        'producer_alias': self.producer_alias,
+                        'message_index': i
+                    }
+                    message = bytes(json.dumps(message), 'ascii')
+                    try:
+                        self.kafka_producer.send(topic=topic, value=message)
+                        self.kafka_producer.flush() # try to empty sending buffer
+                    except KafkaTimeoutError as e:
+                        self.error(f'Timeout error when sending message: {str(e)}')
+                    except Exception as e:
+                        self.error(f'Exception when sending message: {str(e)}')
+                    time.sleep(self.sleep_interval)
+
+        else:
+            self.info('Using custom in-memory data')
+            for i in range(num_messages):
+                self.debug(f'Producing message {i}, stock_symbol={self.stock_symbol}')
+                message = {
+                    'timestamp': datetime.datetime.now().isoformat(),
+                    'stock_symbol': self.stock_symbol,
+                    'current_share_price': yfinance.Ticker(self.stock_symbol).info['currentPrice'],
+                    'producer_alias': self.producer_alias,
+                    'message_index': i
+                }
+                message = bytes(json.dumps(message), 'ascii')
+                try:
+                    self.kafka_producer.send(topic=topic, value=message)
+                    self.kafka_producer.flush() # try to empty sending buffer
+                except KafkaTimeoutError as e:
+                    self.error(f'Timeout error when sending message: {str(e)}')
+                except Exception as e:
+                    self.error(f'Exception when sending message: {str(e)}')
+                time.sleep(self.sleep_interval)
         self.kafka_producer.close ()
